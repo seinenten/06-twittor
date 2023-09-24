41dc848b4a52439ab7fe1716a94874ac83ad8d2f
jorge <20200707@uthh.edu.mx>
2023-09-24 12:18:16 -0600

    modificaciones de iphone x
    modificaciones de iphone x

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 //imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE    = 'static-v3';
-const DYNAMIC_CACHE   = 'dynamic-v2';
+const STATIC_CACHE    = 'static-v4';
+const DYNAMIC_CACHE   = 'dynamic-v3';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 
