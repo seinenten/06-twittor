f431bbfe5fc0d653fc4d495f7216a4c7b998fd27
jorge <20200707@uthh.edu.mx>
2023-09-24 12:11:15 -0600

    ios adaptado
    ios adaptado

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 //imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE    = 'static-v4';
+const STATIC_CACHE    = 'static-v3';
 const DYNAMIC_CACHE   = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
